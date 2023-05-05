d4ebdd5720ca188af0420ebd21ee3fdf85d2052a
hasankhan-123 <hasankha1023@gmail.com>
2023-05-05 22:59:28 +0600

    Best-chef-of-india-clint side complited

--- src/pages/blogPage/BlogPage.jsx
+++ src/pages/blogPage/BlogPage.jsx
@@ -17,7 +17,7 @@ const BlogPage = () => {
                     The key differences between uncontrolled and controlled components are their level of integration with the application's core architecture and their management of state. Uncontrolled components operate independently of the application's state management system, while controlled components are tightly integrated with it. As a result, controlled components are often preferred in larger applications where consistency and predictability are important, while uncontrolled components may be more suitable for simpler, standalone interactions.</p><br />
                 <h3>2. How to validate React props using PropTypes ?</h3>
                 <p>PropTypes is a library in React that provides a convenient way to validate the types of props passed to a component. It allows you to define the expected types of the props, and will throw an error if the actual type of the prop doesn't match the expected type.</p><br />
-                <h3>3. Tell us the difference between nodejs and express js.</h3><br />
+                <h3>3. Tell us the difference between nodejs and express js ?</h3><br />
                 <p>Node.js and Express.js are both important technologies used in web development, but they have different purposes.
 
                     Node.js is a server-side runtime environment that allows developers to use JavaScript to build server-side applications. It provides an event-driven, non-blocking I/O model that makes it lightweight and efficient. Node.js can be used to build a wide range of server-side applications, including web applications, real-time applications, and API services.
